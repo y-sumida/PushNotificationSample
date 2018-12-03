225d10e740ab25f47838eb6646d791f2afdbc64d
Yuki Sumida <y.sumida.0201@gmail.com>
2018-12-03 23:00:14 +0900

    2つ目のVCを実装

---
+++ PushNotificationSample/ViewController2.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController2.swift
+//  PushNotificationSample
+//
+//  Created by Yuki Sumida on 2018/12/03.
+//  Copyright © 2018年 Yuki Sumida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "ViewController2"
+    }
+}
